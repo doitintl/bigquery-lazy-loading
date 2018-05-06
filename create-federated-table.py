c2cf41a995561e09806f5ba0e98650f14733d3c4
Vadim Solovey <vadim@doit-intl.com>
2018-05-06 15:35:23 +0300

    updating project and bucket properties

--- create-federated-table.py
+++ create-federated-table.py
@@ -1,6 +1,6 @@
 #!/bin/python
 from google.cloud import bigquery
-bq_client = bigquery.Client(project='fh-bigquery')
+bq_client = bigquery.Client(project='doit-playground')
 table_ref = bq_client.dataset('views').table('wikipedia_views_gcs')
 table = bigquery.Table(table_ref, schema=SCHEMA)
 extconfig = bigquery.ExternalConfig('CSV')
@@ -11,6 +11,6 @@ extconfig.compression = 'GZIP'
 extconfig.options.allow_jagged_rows = False
 extconfig.options.allow_quoted_newlines = False
 extconfig.max_bad_records = 10000000
-extconfig.source_uris=["gs://fh-bigquery/wikipedia-pagecounts/page*"]
+extconfig.source_uris=["gs://wikimedia-pagecounts/page*"]
 table.external_data_configuration = extconfig
 bq_client.create_table(table)
