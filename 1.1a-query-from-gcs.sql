8cdaa8d69a9cbe382d331ab27a0015fd0fe131bd
Spark 3K <spark3k@MacBook-Pro.local>
2018-05-07 11:53:04 +0300

    adding StandardSQL clause

--- 1.1a-query-from-gcs.sql
+++ 1.1a-query-from-gcs.sql
@@ -1,2 +1,3 @@
+#standardSQL
 SELECT *
 FROM `doit-playground.views.wikipedia_views_gcs` LIMIT 10
--- 1.1b-query-from-gcs.sql
+++ 1.1b-query-from-gcs.sql
@@ -1,2 +1,3 @@
+#standardSQL
 SELECT *, _FILE_NAME fn
 FROM `doit-playground.views.wikipedia_views_gcs` LIMIT 10
