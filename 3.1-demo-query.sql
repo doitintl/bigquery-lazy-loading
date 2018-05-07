242488940768833893d07adb62d13e6a4cf2688f
Vadim Solovey <vadim@doit-intl.com>
2018-05-07 12:35:07 +0300

    fixing table and dataset names

--- 3.1-demo-query.sql
+++ 3.1-demo-query.sql
@@ -1,5 +1,5 @@
 #standardSQL
 SELECT datehour, title, views
-FROM `doit-playground.wikipedia_v2.pageviews_2018`
-WHERE DATE(datehour) BETWEEN "2018-02-10" AND "2018-02-28"
+FROM `doit-playground.wikipedia_v2.pageviews_2016`
+WHERE DATE(datehour) BETWEEN "2016-02-10" AND "2016-02-28"
 AND wiki = 'en'AND title IN ('Curling', 'Bobsleigh')
