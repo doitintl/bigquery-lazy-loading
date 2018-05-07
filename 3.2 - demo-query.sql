5eb2a228c7689ff6ae8ddd703322b1f5b1880b39
Vadim Solovey <vadim@doit-intl.com>
2018-05-07 09:26:54 +0300

    Create 3.2 - demo-query.sql

---
+++ 3.2 - demo-query.sql
@@ -0,0 +1,6 @@
+#standardSQL
+SELECT datehour, title, views
+FROM `doit-playground.wikipedia_v2.pageviews_2018`
+WHERE DATE(datehour) BETWEEN "2018-02-10" AND "2018-02-28" 
+AND wiki = 'en.m'
+AND title IN ('Curling', 'Bobsleigh')
