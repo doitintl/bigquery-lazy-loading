532185501cb72cd3588a9177dea16112088f99c7
Vadim Solovey <vadim@doit-intl.com>
2018-05-07 09:20:10 +0300

    Create 1.2 - parse-table.sql

---
+++ 1.2 - parse-table.sql
@@ -0,0 +1,12 @@
+#standardSQL
+CREATE VIEW `doit-playground.views.wikipedia_views_test_ddl`
+AS SELECT
+  PARSE_TIMESTAMP('%Y%m%d-%H%M%S', REGEXP_EXTRACT(_FILE_NAME, '[0-9]+-[0-9]+')) datehour
+  , REGEXP_EXTRACT(line, '([^ ]*) ') wiki
+  , REGEXP_EXTRACT(line, '[^ ]* (.*) [0-9]+ [0-9]+') title
+  , CAST(REGEXP_EXTRACT(line, ' ([0-9]+) [0-9]+$') AS INT64) views
+  , CAST(REGEXP_EXTRACT(line, ' ([0-9]+)$') AS INT64) zero
+  , _FILE_NAME filename
+  , line
+FROM `doit-playground.views.wikipedia_views_gcs`WHERE REGEXP_EXTRACT(line, ' ([0-9]+) [0-9]+$') IS NOT NULL # views
+AND REGEXP_EXTRACT(line, ' ([0-9]+)$') = '0' # zero
