d3b9cbb61a24c83bb0732d66b62de224cc151119
Vadim Solovey <vadim@doit-intl.com>
2018-05-08 09:42:38 +0300

    Update 1.4-be-careful.sql

--- 1.4-be-careful.sql
+++ 1.4-be-careful.sql
@@ -1,4 +1,4 @@
 #standardSQL
 SELECT SUM(views) views
   , COUNT(DISTINCT filename) files
-  , COUNT(*) n_rows FROM `doit-playground.views.wikipedia_views_test_ddl`
+  , COUNT(*) n_rows FROM `doit-playground.views.wikipedia_views_gcs_parsed`
