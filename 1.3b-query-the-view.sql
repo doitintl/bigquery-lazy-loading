7cdff7858e893d4ed7e91685619d80ffb2776433
Vadim Solovey <vadim@doit-intl.com>
2018-05-08 09:41:02 +0300

    Update 1.3b-query-the-view.sql

--- 1.3b-query-the-view.sql
+++ 1.3b-query-the-view.sql
@@ -1,7 +1,7 @@
 #standardSQL
 SELECT COUNT(*) n_rows, SUM(views) views
   , ARRAY_AGG(DISTINCT filename) files
-FROM `doit-playground.views.wikipedia_views_test_ddl`
+FROM `doit-playground.views.wikipedia_views_gcs_parsed`
 WHERE EXTRACT(YEAR FROM datehour)=2015
 AND EXTRACT(MONTH FROM datehour)=10
 AND EXTRACT(DAY FROM datehour)=21
